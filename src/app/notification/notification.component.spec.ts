48deb504d382cd1c351518a0a298abb4eab96531
Jonas Van Goolen <jonas@atmire.com>
2017-09-05 15:52:20 +0200

    41174 Additional components testing setup

--- src/app/notification/notification.component.spec.ts
+++ src/app/notification/notification.component.spec.ts
@@ -2,10 +2,17 @@ import { NotificationComponent } from './notification.component'
 import { NgbAlert, NgbAlertConfig } from '@ng-bootstrap/ng-bootstrap';
 import { ComponentFixture, TestBed, async } from '@angular/core/testing';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { Notification } from './notification.model';
+import { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
 
 describe('NotificationComponent', () => {
-  let comp = NotificationComponent;
+  let comp: NotificationComponent;
   let fixture: ComponentFixture<NotificationComponent>;
+
+  let de: DebugElement;
+  let el: HTMLElement;
+
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -17,8 +24,12 @@ describe('NotificationComponent', () => {
 
   beforeEach(() => {
     fixture = TestBed.createComponent(NotificationComponent);
-    // comp = fixture.componentInstance;
+    comp = fixture.componentInstance;
+    de = fixture.debugElement.query(By.css('div.toggle-container > ngb-alert'));
+    el = de.nativeElement;
   });
   it('should display the component', () => {
+    comp.notification = new Notification();
+    fixture.detectChanges();
   })
 })
--- src/app/notification/notifications-wrapper.component.spec.ts
+++ src/app/notification/notifications-wrapper.component.spec.ts
@@ -4,25 +4,48 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NotificationComponent } from './notification.component';
 import { NotificationService } from './notification.service';
-import { Store } from '@ngrx/store';
+import { Store, StoreModule } from '@ngrx/store';
+import { NotificationState } from './state/notification.reducer';
+import { NotificationType } from './notification.model';
+import { Notification } from './notification.model';
+import { UUID } from 'angular2-uuid';
+import { Observable } from 'rxjs/Observable';
+import { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
 
 describe('NotificationComponent', () => {
-  let comp = NotificationsWrapperComponent;
+  let comp: NotificationsWrapperComponent;
   let fixture: ComponentFixture<NotificationsWrapperComponent>;
+  let existingNotification: Notification = new Notification();
+  existingNotification.message = 'Existing notification';
+  existingNotification.type = NotificationType.INFO;
+  existingNotification.dismissible = true;
+  existingNotification.id = UUID.UUID();
+  const initialState: NotificationState = {
+    notifications: [ existingNotification ]
+  }
+  let store: Store<NotificationState> = new Store<NotificationState>(undefined, undefined, Observable.of(initialState));
+
+  let de: DebugElement;
+  let el: HTMLElement;
 
   beforeEach(async(() => {
+
     TestBed.configureTestingModule({
       declarations: [ NotificationsWrapperComponent, NotificationComponent, NgbAlert ],
       imports: [ BrowserAnimationsModule ],
-      providers: [ NgbAlertConfig, NotificationService, Store]
+      providers: [
+        NgbAlertConfig,
+        { provide: NotificationService, useValue: new NotificationService(store) },
+      ],
     }).compileComponents();
 
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(NotificationsWrapperComponent);
-    // comp = fixture.componentInstance;
+    comp = fixture.componentInstance;
   });
-  it('should display the component', () => {
-  })
+
 })
+
